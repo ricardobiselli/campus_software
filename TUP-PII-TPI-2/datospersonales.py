39f05dc37a1879ecd8c2175752a00829e4bc5c4e
ricardobiselli <ricardobiselli@gmail.com>
2023-10-20 00:05:22 -0300

    modificación módulo registro

--- TUP-PII-TPI-2/datospersonales.py
+++ TUP-PII-TPI-2/datospersonales.py
@@ -8,17 +8,3 @@ registro = [
              "mercedes@gmail.com", "mercedes123"),  # usuario pre-cargado
 ]
 
-cursos = [
-    {"Materia": "Ingles I",
-     "Carrera": "Tecnicatura Universitaria en Programación"},
-    {"Materia": "Ingles II",
-     "Carrera": "Tecnicatura Universitaria en Programación"},
-    {"Materia": "Laboratorio I",
-     "Carrera": "Tecnicatura Universitaria en Programación"},
-    {"Materia": "Laboratorio II",
-     "Carrera": "Tecnicatura Universitaria en Programación"},
-    {"Materia": "Programación I",
-     "Carrera": "Tecnicatura Universitaria en Programación"},
-    {"Materia": "Programación II",
-     "Carrera": "Tecnicatura Universitaria en Programación"}
-]
